5ca33d5abc58c6de6a0b1d1fbc24afd52f7bf24d
Jayanti Jagannath <jagannath.jyanti5@gmail.com>
2020-08-31 19:40:49 +0530

    image bitmap generated

--- app/src/main/java/com/example/googlelensclone/MainActivity.kt
+++ app/src/main/java/com/example/googlelensclone/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.googlelensclone
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -10,6 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         @JvmStatic val REQ_CODE = 123
+        @JvmStatic val EXTRA_DATA = "data"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,5 +23,17 @@ class MainActivity : AppCompatActivity() {
             val takePhotoIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             startActivityForResult(takePhotoIntent, REQ_CODE)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
+
+
+        if(requestCode == REQ_CODE){
+            val bitmap = data?.extras?.get(EXTRA_DATA) as Bitmap
+            img.setImageBitmap(bitmap)
+            return
+        }
+
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
